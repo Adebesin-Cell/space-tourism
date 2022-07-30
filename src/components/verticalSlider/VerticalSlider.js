58d86bc7837a4b133a09e32a9af33f210a9ac1c8
Adebesin Cell <adebesintolulope80@gmail.com>
2022-07-30 08:25:21 +0100

    Updating UI

--- src/components/verticalSlider/VerticalSlider.js
+++ src/components/verticalSlider/VerticalSlider.js
@@ -1,7 +1,85 @@
-import styles from "./VerticalSlider.module.scss";
+import styles from './VerticalSlider.module.scss';
+import { useState } from 'react';
+import { useSwipeable } from 'react-swipeable';
 
 const VerticalSlider = function (props) {
-  return <div className={styles.container}>&nbsp;</div>;
+  const [activeIndex, setActiveIndex] = useState(0);
+
+  const handlers = useSwipeable({
+    onSwipedLeft: () => {
+      updateIndex(activeIndex + 1);
+    },
+    onSwipedRight: () => {
+      updateIndex(activeIndex - 1);
+    },
+    onSwipedUp: () => {
+      updateIndex(activeIndex + 1);
+    },
+    onSwipedDown: () => {
+      updateIndex(activeIndex - 1);
+    },
+  });
+
+  if (!props.data) {
+    return <p style={{ color: 'white' }}>Loading...</p>;
+  }
+
+  const switchIndexHandler = function (e) {
+    const { index } = e.target.dataset;
+    setActiveIndex(+index);
+  };
+
+  function updateIndex(newIndex) {
+    if (newIndex < 0) {
+      newIndex = 0;
+    } else if (newIndex >= props.data.length) {
+      newIndex = props.data.length - 1;
+    }
+
+    setActiveIndex(newIndex);
+  }
+
+  return (
+    <section className={styles.container}>
+      <div className={styles.slider} {...handlers}>
+        <div className={styles.slider__buttonContainer}>
+          <div className={styles.slider__buttonWrapper}>
+            {props.data.map((_, i) => (
+              <button
+                key={i}
+                data-index={i}
+                className={`${styles.slider__button} ${
+                  activeIndex === i ? styles['slider__button--active'] : ''
+                }`}
+                onClick={switchIndexHandler}
+              >
+                {i + 1}
+              </button>
+            ))}
+          </div>
+        </div>
+        <div className={styles.slider__wrap}>
+          <div
+            className={styles.slider__inner}
+            style={{ transform: `translateY(-${activeIndex * 100}%)` }}
+          >
+            {props.data.map((item, i) => (
+              <div key={i} className={styles.slider__item}>
+                <div className={styles.sldier__content}>&nbsp;</div>
+                <div className={styles['slider__image--box']}>
+                  <img
+                    src={item.images.portrait}
+                    alt={item.nam}
+                    className={styles.slider__image}
+                  />
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default VerticalSlider;
